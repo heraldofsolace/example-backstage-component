fc100a173506c26ed753fea744b4f2ad7251090d
Aniket Bhattacharyea <aniket@abhattacharyea.dev>
2023-12-08 19:20:00 +0530

    Update lambda_function.py

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,5 +1,7 @@
 def greet(name):
+    api_key = "hvs.jWoTNpN6X4X9ERfBxkPqzPgk"
     print("Hello, %s" % name)
+    print("Your api key is: %s" % api_key)
 
 name = input("Please enter your name: ")
 exec("greet('%s')" % name)

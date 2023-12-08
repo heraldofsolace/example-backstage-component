c0c1f83be151df6c47e169f3a4ba35699c116180
Aniket Bhattacharyea <aniket@abhattacharyea.dev>
2023-12-08 18:47:59 +0530

    Update lambda_function.py

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,4 +1,5 @@
 def lambda_handler(event, context):  
-  api_key = "hvs.jWoTNpN6X4X9ERfBxkPqzPgk"
-  print(api_key)
-  print("Hello from Backstage!")
+  if event['queryStringParameters']['password'] == 'myPassword':
+    print("Hello from Backstage!")
+  else:
+    print("Not allowed")

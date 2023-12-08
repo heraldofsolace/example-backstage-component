0236f670025068e635cf99b676298e2358d3ab7c
Aniket Bhattacharyea <aniket@abhattacharyea.dev>
2023-12-08 19:31:38 +0530

    Update lambda_function.py

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,13 +1,8 @@
-import os
+import re
 
-
-def greet(name):
-    api_key = "hvs.jWoTNpN6X4X9ERfBxkPqzPgk"
-    print("Hello, %s" % name)
-    print("Your api key is: %s" % api_key)
-
-
-print(f"[INFO] Environment: {os.environ}")
-
-name = input("Please enter your name: ")
-exec("greet('%s')" % name)
+def filterScriptTags(content): 
+    oldContent = ""
+    while oldContent != content:
+        oldContent = content
+        content = re.sub(r'<script.*?>.*?</script>', '', content, flags= re.DOTALL | re.IGNORECASE)
+    return content
